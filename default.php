c6b6680ed5ffbc22c2c008ae92a0910429459534
unknown <200389459@student.georgianc.on.ca>
2018-12-13 13:43:47 -0500

    github address edit

--- default.php
+++ default.php
@@ -6,7 +6,7 @@ require('header.php');
 <main class="well text-center">
     <h1 class="text-center">Travel Bug</h1>
     <p>This site was created for COMP1006 Intro to Web Programming in Fall 2018 at Georgian College. It uses PHP, MySQL, and twitter Bootstrap and is deployed to AWS.</p>
-    <p>Source Code is available at <a href="https://github.com/sehee22/comp1006-final" target="_blank">https://github.com/sehee22/comp1006-final</a></p>
+    <p>Source Code is available at <a href="https://github.com/sehee22/comp1006-fina" target="_blank">https://github.com/sehee22/comp1006-fina</a></p>
 </main>
 
 <?php require('footer.php'); ?>
